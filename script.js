e82b04e8e60357139db25ea0f2eb3ee3eefbae8b
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-04-25 22:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+
+const openModalButton = document.getElementById('open-modal');
+const newtonModal = new bootstrap.Modal(document.getElementById('newton-modal'));
+
+openModalButton.addEventListener('click', () => {
+  newtonModal.show();
+});
